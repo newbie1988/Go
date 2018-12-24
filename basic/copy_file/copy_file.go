6390bd5f74c6be49d391b71070418f3bdbf92f4c
newbie@1988 <312462392@qq.com>
2018-12-24 18:44:25 +0800

    add io.Copy for copy file

--- basic/copy_file/copy_file.go
+++ basic/copy_file/copy_file.go
@@ -9,7 +9,26 @@ import (
 const (
 	InputFileName = "test_in.data"
 	OutputFileName = "test_out.data"
+	OutputFileName1 = "test_out1.data"
 )
+
+func CopyFile() {
+	input, inErr := os.Open(InputFileName);
+	if(nil != inErr) {
+		fmt.Println("Open", InputFileName, "Failed!");
+		return;
+	}
+	defer input.Close();
+	
+	output, outErr := os.Create(OutputFileName1);
+	if(nil != outErr) {
+		fmt.Println("Open", OutputFileName1, "Failed!");
+		return;
+	}
+	
+	defer output.Close();
+	io.Copy(output, input);
+}
 
 func main() {
 	input, inErr := os.Open(InputFileName);
@@ -37,4 +56,6 @@ func main() {
 	}
 	// need flush before close. flush the buffer to file.
 	outputWriter.Flush();
+	
+	CopyFile();
 }
